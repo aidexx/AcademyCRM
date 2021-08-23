f34c56c564a2489c14aba07d30ee0b1a051e66ab
aidexx <yermakovich.nikita@gmail.com>
2021-08-23 17:04:53 +0300

    Added manage  page layout

---
+++ AcademyCRM/AcademyCRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AcademyCRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
